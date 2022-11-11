af4f74ed62a4eeab5b76f7f49e4b5674769f6b54
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-11-10 19:40:42 -0600

    Commented out login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,49 +1,49 @@
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+// const loginFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const email = document.querySelector("#login-email").value.trim();
-  const password = document.querySelector("#login-pwd").value.trim();
+//   const email = document.querySelector("#login-email").value.trim();
+//   const password = document.querySelector("#login-pwd").value.trim();
 
-  if (email && password) {
-    const response = await fetch("/api/member/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+//   if (email && password) {
+//     const response = await fetch("/api/member/login", {
+//       method: "POST",
+//       body: JSON.stringify({ email, password }),
+//       headers: { "Content-Type": "application/json" },
+//     });
 
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to log in.");
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert("Failed to log in.");
+//     }
+//   }
+// };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
+// const signupFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const email = document.querySelector("#create-email").value.trim();
-  const password = document.querySelector("#create-pwd").value.trim();
+//   const email = document.querySelector("#create-email").value.trim();
+//   const password = document.querySelector("#create-pwd").value.trim();
 
-  if (email && password) {
-    const response = await fetch("/api/member/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+//   if (email && password) {
+//     const response = await fetch("/api/member/login", {
+//       method: "POST",
+//       body: JSON.stringify({ email, password }),
+//       headers: { "Content-Type": "application/json" },
+//     });
 
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to sign up.");
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert("Failed to sign up.");
+//     }
+//   }
+// };
 
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+// document
+//   .querySelector(".login-form")
+//   .addEventListener("submit", loginFormHandler);
 
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+// document
+//   .querySelector(".signup-form")
+//   .addEventListener("submit", signupFormHandler);
